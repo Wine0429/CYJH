a4e815743a2d1acca379584ec010e95e3795e146
Wine0429 <161473758+Wine0429@users.noreply.github.com>
2024-03-12 21:23:26 +0800

    0312exercise
    exercise5

---
+++ script.js
@@ -0,0 +1,13 @@
+document.write("I will take the faith");
+function sayHello(){
+    var response = prompt("What is your name?");
+    alert(response+",I'm movin on");
+
+  };
+  //sayHello();
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+};
